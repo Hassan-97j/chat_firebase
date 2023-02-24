bb9c760a2dfb58412c0a53ed81d9626398a9a294
Hassaan Junaid <hassaanjunaid.soomro@gmail.com>
2023-02-23 18:09:38 -0600

    commit

--- lib/presentation/sign_in/controller.dart
+++ lib/presentation/sign_in/controller.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
-import 'package:chat_firebase/app/utils/loading.dart';
 import 'package:get/get.dart';
-// import '../../app/utils/dialogs.dart';
+import '../../app/utils/dialogs.dart';
 import '../../domain/repositories/firebase_repo/auth_repo_impl.dart';
 import '../../routes/routes.dart';
 import '../../data/repositories/firebase_repo/auth_repository.dart';
@@ -18,22 +17,20 @@ class SignInController extends GetxController {
 //sign in function
   Future<void> handleSignIn() async {
     try {
-      // Dialogs.showProgressBar();
-      Loading.show('loading...');
+       Dialogs.showProgressBar();
+      
       await InternetAddress.lookup('google.com');
       await authRepository.signInWithGoogle();
-      Loading.dismiss();
-      //Dialogs.hideProgressBar();
+   
+      Dialogs.hideProgressBar();
       Get.offAndToNamed(AppRoutes.application);
-      Loading.toast("Login Success");
-      //Dialogs.showSnackbar('succes', "'Login Success");
+  
+      Dialogs.showSnackbar('succes', "'Login Success");
     } catch (e) {
       // ignore: avoid_print
       print('error: $e');
-      //Dialogs.hideProgressBar();
-      Loading.dismiss();
-      Loading.toast("Login Success");
-      //Dialogs.showSnackbar('error', "No Internet Connection");
+      Dialogs.hideProgressBar();
+      Dialogs.showSnackbar('error', "No Internet Connection");
     }
     update();
   }
